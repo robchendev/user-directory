a4a3441d44942916497a169a12c1169467d9a4a5
robchendev <robchendev@gmail.com>
2025-07-08 12:29:21 -0700

    search phone number as well

--- src/components/UserSearch/UserSearch.utils.ts
+++ src/components/UserSearch/UserSearch.utils.ts
@@ -2,5 +2,5 @@ import type { User } from "../../types/user";
 
 export const searchUserName = (user: User, search: string) => {
   const searchTerm = search.toLowerCase();
-  return user.name.toLowerCase().includes(searchTerm);
+  return user.name.toLowerCase().includes(searchTerm) || user.phone.replace(/[-.()]/g, "").includes(searchTerm) || user.phone.includes(searchTerm);
 };
